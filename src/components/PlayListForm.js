3f2ac5f5e263626ec3a3b71e47fc1835303990e2
Craig Wendel <wendelcraig@gmail.com>
2017-07-21 16:21:32 -0500

    Add Nav and Forms w/ BootStrap.

--- src/components/PlayListForm.js
+++ src/components/PlayListForm.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react'
+const submitStyle = {
+  color: 'white',
+  backgroundColor: 'red',
+  border: 0
+}
 
 export default class PlayListForm extends Component {
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      value: ''
+    }
+  }
+
   render () {
     return (
-      <div>PlayListForm</div>
+      <div>
+        <div className='playlist-form'>
+        <form>
+        <div className="form-group">
+          <label htmlFor="formGroupExampleInput">Username:</label>
+          <input value={this.state.value} onChange={this.handleUsernameChange} type="text" className="form-control" id="formGroupExampleInput" placeholder="Enter your username..." />
+        </div>
+        <div className="form-group">
+          <label htmlFor="formGroupExampleInput2">Artist/Band</label>
+          <input value={this.state.value} onChange={this.handleArtistChange} type="text" className="form-control" id="formGroupExampleInput2" placeholder="Enter the Band or Artist name..." />
+        </div>
+        <div className="form-group">
+          <label htmlFor="formGroupExampleInput">Song Title:</label>
+          <input value={this.state.value} onChange={this.handleTitleChange} type="text" className="form-control" id="formGroupExampleInput" placeholder="Enter the song title..." />
+        </div>
+        <div className="form-group">
+          <label htmlFor="formGroupExampleInput2">Song Notes:</label>
+          <input value={this.state.value} onChange={this.handleNotesChange} type="text" className="form-control" id="formGroupExampleInput2" placeholder="Enter some notes about the song..." />
+        </div>
+        <div>
+          <button style={submitStyle} type="submit" className="btn btn-primary">Submit</button>
+        </div>
+        </form>
+      </div>
+      </div>
     )
   }
 }
