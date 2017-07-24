642c75e26c546152deb9fef7d838b51e0b90a209
Craig Wendel <wendelcraig@gmail.com>
2017-07-24 00:21:00 -0500

    Add functionality to pass PlayListItem component into the playlist screen. Need to work on layout with bootstrap.

--- src/components/PlayList.js
+++ src/components/PlayList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-// import PlayListItem from './PlayListItem'
+import PlayListItem from './PlayListItem'
 
 export default class PlayList extends Component {
   constructor (props) {
@@ -14,7 +14,7 @@ export default class PlayList extends Component {
 
   componentDidMount () {
     fetch('https://tiny-lasagna-server.herokuapp.com/collections/playlisting').then(results => {
-        return results.json();
+        return results.json()
       }).then(data => {
         this.setState({songs: data})
       })
@@ -22,12 +22,10 @@ export default class PlayList extends Component {
 
   fetchData (event) {
    event.preventDefault()
-   console.log('clicked update')
    fetch('https://tiny-lasagna-server.herokuapp.com/collections/playlisting').then(results => {
      return results.json()
    }).then(data => {
      this.setState({songs: data})
-     console.log('clicked update')
    })
   }
 
@@ -39,18 +37,11 @@ export default class PlayList extends Component {
           <button type="submit" className="btn btn-success">Update</button>
           </form>
         </div>
-        <div className='playlist'>
-          {this.state.songs.map( (result) =>
-          <div className="card" key={result._id}>
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">Username: {result.userName}</li>
-              <li className="list-group-item">Artist/Band: {result.songArtist}</li>
-              <li className="list-group-item">Song Title: {result.songTitle}</li>
-              <li className="list-group-item">Song notes: {result.songNotes}</li>
-            </ul>
-          </div>
-          )}
+        <div>
+          <PlayListItem songs={this.state.songs} />
         </div>
+
+
       </div>
     )
   }
--- src/components/PlayListItem.js
+++ src/components/PlayListItem.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
 
 export default class PlayListItem extends Component {
+  
   render () {
     return (
-      <div>PlayListItem</div>
+      <div>
+        <div className='playlist'>
+          {this.props.songs.map( (result) =>
+          <div className="card" key={result._id}>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">Username: {result.userName}</li>
+              <li className="list-group-item">Artist/Band: {result.songArtist}</li>
+              <li className="list-group-item">Song Title: {result.songTitle}</li>
+              <li className="list-group-item">Song notes: {result.songNotes}</li>
+            </ul>
+          </div>
+          )}
+      </div>
+    </div>
     )
   }
 }
