428c83a6063294487555ec20620775f12240f07f
Craig Wendel <wendelcraig@gmail.com>
2017-07-24 23:26:58 -0500

    Adjust nav bar to be fixed and stay at the top of screen at all times.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ export default class NavBar extends Component {
   render () {
     return (
       <div>
-        <nav className="navbar sticky-top navbar-light bg-faded">
+        <nav className="navbar fixed-top navbar-light bg-faded">
           <span>
             <img src={logo} width="30" height="30" className="d-inline-block align-top" alt="Logo" />
             <a className="navbar-brand" href="/">Fire Jams!</a>
